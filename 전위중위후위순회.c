c27a2d5a67916ba58b250b8125d4b43b0b79e811
wjddnchl33 <152261398+wjddnchl33@users.noreply.github.com>
2023-11-28 17:14:24 +0900

    전위중위후위 순회
    전위중위후위 순회

---
+++ 전위중위후위순회.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#define DataType char
+
+typedef struct Tnode {
+    struct Tnode *left;
+    DataType data;
+    struct Tnode *right;
+} Tnode;
+
+typedef struct linkedBT{
+    Tnode *root;
+}LinkedBT;
+
+preoder(Tnode *root) {
+    if(root) {
+        printf("%c ", root->data);
+        preoder(root->left);
+        preoder(root->right);
+    }
+}
+
+inorder(Tnode *root) {
+    if(root) {
+        inorder(root->left);
+        printf("%c ", root->data);
+        inorder(root->right);
+    }
+}
+
+postorder(Tnode *root) {
+    if(root) {
+        postorder(root->left);
+        postorder(root->right);
+        printf("%c ", root->data);
+    }
+}
+
+int main() {
+    Tnode node9 = {NULL, 'B', NULL};
+    Tnode node8 = {NULL, 'A', NULL};
+    Tnode node7 = {NULL, 'E', NULL};
+    Tnode node6 = {NULL, 'D', NULL};
+    Tnode node5 = {NULL, 'C', NULL};
+    Tnode node4 = {&node8, '-', &node9};
+    Tnode node3 = {&node6, '/', &node7};
+    Tnode node2 = {&node4, '*', &node5};
+    Tnode node1 = {&node2, '+', &node3};
+    preoder(&node1); printf("<-전위 순회\n");
+    inorder(&node1); printf("<-중위 순회\n");
+    postorder(&node1); printf("<-후위 순회\n");
+
+
+}
